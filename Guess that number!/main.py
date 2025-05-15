862636f967de9c8cd522ead01e93283e98b8bc04
Peaklatice-9348 <yusufjavaid.072012@gmail.com>
2025-05-15 19:58:28 +0100

    Add files via upload

---
+++ Guess that number!/main.py
@@ -0,0 +1,17 @@
+from tkinter import *
+import math
+import tkinter.messagebox
+import random
+
+window = Tk()
+window.title('game')
+
+label = Label(window,text='welcome to...',font=('arial',20))
+label2 = Label(window,text='GUESS THAT NUMBER!!!!!',font=('impact',100))
+label.grid(row=1,column=1)
+label2.grid(row=2,column=1)
+
+
+
+
+window.mainloop()
